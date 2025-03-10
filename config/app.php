40e524bed0c5a152789f5cd0107f494de32b13d1
georges-bc <g.bouchedid@fleetrunnr.com>
2025-03-10 17:54:52 +0200

    last one 3

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://wedding-appointment-9b195979be8c.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
